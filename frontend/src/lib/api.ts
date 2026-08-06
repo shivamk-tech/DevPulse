8f23b716671083155c3b6eb59fb73377849648ed
shivamk-tech <skkumarsv786@gmail.com>
2026-08-06 15:24:00 +0530

    Axios interface

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -2,41 +2,46 @@ import axios from "axios";
 import { HOST } from '@/config/host'
 import { error } from "console";
 import { promise } from "zod";
+import { InternalAxiosRequestConfig } from "axios";
 
-if(!HOST.backend) {
+if (!HOST.backend) {
     throw new Error("NEXT_PUBLIC_API_URL is not defined ")
 }
 
 export const api = axios.create({
-    baseURL:HOST.api,
+    baseURL: HOST.api,
 
-    headers:{
-        "Content-Type":"application/json"
+    headers: {
+        "Content-Type": "application/json"
     },
 
-    withCredentials:true
+    withCredentials: true
 })
 
 let isRefreshing = false;
 
 type FailedRequest = {
     resolve: () => void;
-    reject: (error: unknown) => void; 
+    reject: (error: unknown) => void;
 };
+
+interface RetryAxiosConfigRequest extends InternalAxiosRequestConfig {
+    
+}
 
 let failedQueue: FailedRequest[] = [];
 
 
 function processQueue(error?: unknown) {
-  failedQueue.forEach(({ resolve, reject }) => {
-    if (error) {
-      reject(error);
-    } else {
-      resolve();
-    }
-  });
+    failedQueue.forEach(({ resolve, reject }) => {
+        if (error) {
+            reject(error);
+        } else {
+            resolve();
+        }
+    });
 
-  failedQueue = [];
+    failedQueue = [];
 }
 
 api.interceptors.response.use(
@@ -44,7 +49,19 @@ api.interceptors.response.use(
         return response;
     },
     async (error) => {
-        return Promise.reject(error);
+
+        if (error.response?.status !== 401) {
+            return Promise.reject(error);
+        }
+        
+        const originalRequest = error.config
+
+        if(originalRequest._retry){
+            return Promise.reject(error);
+        }
+
+
+
     }
 );
 
