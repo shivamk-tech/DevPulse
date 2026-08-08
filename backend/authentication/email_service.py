9bd6e98fa27be33eba0954a3570ee7b8592ea261
shivamk-tech <skkumarsv786@gmail.com>
2026-08-08 18:03:16 +0530

    reset-password

--- backend/authentication/email_service.py
+++ backend/authentication/email_service.py
@@ -1,60 +1,60 @@
-from django.core.mail import send_mail, EmailMultiAlternatives
 from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
-from .utils import generate_verification_token
+
 from .utils import generate_verification_link
 
-def send_verification_email(user):
-    verification_link = generate_verification_link(user)
 
-    subject = "Verify your email verification"
-
-    message = f"""
-Hi {user.first_name},
-
-Welcome to Beacon!
-
-Please verify your email by clicking the link below:
-
-{verification_link}
-
-If you didn't create this account, you can safely ignore this email.
-
-- Beacon Team
-"""
-
-    send_mail(
-        subject=subject,
-        message=message,
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=[user.email],
-        fail_silently=False,
-    )
-
-def send_password_reset_email(user, reset_link):
-    subject = "Reset your Beacon Password"
-
-    context = {
-        "user": user,
-        "reset_link": reset_link,
-    }
-
+def _send_email(
+    *,
+    subject: str,
+    template_name: str,
+    context: dict,
+    recipient: str,
+):
     html_message = render_to_string(
-        "emails/password_reset.html",
+        f"emails/{template_name}.html",
         context,
     )
 
     text_message = render_to_string(
-        "emails/password_reset.txt",
+        f"emails/{template_name}.txt",
         context,
     )
 
     email = EmailMultiAlternatives(
         subject=subject,
         body=text_message,
-        from_email=settings.DEFAULT_FROM_USER,
-        to=[user.email],
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=[recipient],
     )
 
     email.attach_alternative(html_message, "text/html")
+
     email.send()
+
+
+def send_verification_email(user):
+    verification_link = generate_verification_link(user)
+
+    _send_email(
+        subject="Verify your DevPulse account",
+        template_name="verification",
+        context={
+            "user": user,
+            "verification_link": verification_link,
+        },
+        recipient=user.email,
+    )
+
+
+def send_password_reset_email(user, reset_link):
+    _send_email(
+        subject="Reset your DevPulse password",
+        template_name="password_reset",
+        context={
+            "user": user,
+            "reset_link": reset_link,
+        },
+        recipient=user.email,
+    )
